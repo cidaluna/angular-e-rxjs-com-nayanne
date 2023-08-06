1ca5c0b6018a42cf170286f3ab5c875e244fae8b
cidaluna <cidalunna@hotmail.com>
2023-08-06 14:28:56 -0300

    Using pipe, tap, map to manipulate data from API response

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LivrosResultado } from '../models/interfaces';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +16,19 @@ export class LivroService {
   constructor(private http: HttpClient) { }
 
   
-  buscar(valorDigitado: string): Observable<LivrosResultado>{
+  buscar(valorDigitado: string): Observable<Item[]>{
     // Quando o usuário digitar uma informação e clicar na lupa, o valor digitado será o parâmetro
     const params = new HttpParams().append('q', valorDigitado);
     // Chamando a API e passando o parâmetro
     // Tipando a Interface no método get<>()
-    return this.http.get<LivrosResultado>(this.API, { params })
+    // Utilizando pipe para manipular as informações recebidas, como uma forma de canalizar os dados
+    // O tap não modifca os dados, serve apenas para visualizá-los
+    // O map sim, manipula o dado e traz o array de items definido na interface
+    return this.http.get<LivrosResultado>(this.API, { 
+      params }).pipe(
+        tap(retornoDadosAPI => console.log('Fluxo do tap: ', retornoDadosAPI)),
+        map(resultado => resultado.items),
+        tap(resultado => console.log('Fluxo após o map: ', resultado))
+      )
   }
 }
