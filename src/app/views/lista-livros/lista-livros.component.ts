7e044766c4582a574d17a233161fc536d972a744
cidaluna <cidalunna@hotmail.com>
2023-08-06 15:19:07 -0300

    Render cards on screen

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -9,9 +10,11 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy{
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = ''
   subscription: Subscription;  // para tornar possível o desinscrever 
+  livro: Livro;
+
   // Injetando o service para possibilitar o uso dos recursos e métodos da API 
   constructor(private service: LivroService) { }
 
@@ -22,12 +25,42 @@ export class ListaLivrosComponent implements OnDestroy{
       // (error) => console.log('Teste chamando API ', error)
       {
         // a notificação next pode ser emitida várias vezes (traz os dados)
-        next: retornoAPI => console.log('Teste chamando a API: ', retornoAPI),
+        //next: retornoAPI => console.log('Teste chamando a API: ', retornoAPI),
+        next: (items) =>{
+          this.listaLivros = this.livrosResultadoParaLivros(items)
+        },
         // a notificação error e complete só permite ser emitida apenas uma vez
-        error: erro => console.log('Ocorreu o erro: ', erro), 
-        complete: () => console.log('Observable completado!!')
+        error: erro => console.log('Ocorreu o erro: ', erro)
+        //complete: () => console.log('Observable completado!!')
       }
       )
+  }
+
+  // https://angular.io/guide/http-request-data-from-server#requesting-a-typed-response
+  // Converter o objeto json para o tipo de dado necessário, podendo acessar as propriedades definidas na interface
+  // Exemplo:
+  //  .subscribe(data => this.config = {
+  //  heroesUrl: (data as any).heroesUrl,
+  //  textfile:  (data as any).textfile,
+  //  });
+
+  livrosResultadoParaLivros(items): Livro[] {
+    // definir uma lista de livros e adicionar(push) cada livro na lista
+    const livros: Livro[] = []
+
+    // passar por cada livro e atribuir as propriedades
+    items.forEach(item => {
+      livros.push(this.livro = {
+                title: item.volumeInfo?.title,
+                authors: item.volumeInfo?.authors,
+                publisher: item.volumeInfo?.publisher,
+                publishedDate: item.volumeInfo?.publishedDate,
+                description: item.volumeInfo?.description,
+                previewLink: item.volumeInfo?.previewLink,
+                thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+              })
+    })
+    return livros
   }
 
   ngOnDestroy(){
